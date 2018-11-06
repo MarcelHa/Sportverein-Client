533bfc7485ed0818be44ca6b04882bb4989b7d17
lukas-v95 <lukas1995@nurfuerspam.de>
2018-11-06 23:05:28 +0100

    removed FXML warnings
     +if members Table empty -->set default text

--- CMS_Client/src/main/java/rmi/RMIClient.java
+++ CMS_Client/src/main/java/rmi/RMIClient.java
@@ -14,7 +14,7 @@ import java.rmi.RemoteException;
 public class RMIClient {
 
     //private static String serverIP = "172.22.8.115";
-    private static String serverIP = "172.22.8.115";
+    private static String serverIP = "172.23.8.157";
     private static RMIClient rmiClient;
 
 
